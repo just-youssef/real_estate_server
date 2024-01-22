8e5618bdd3f1209e7293e839fb46dc409fe88dac
Youssef Hussein <61589133+just-youssef@users.noreply.github.com>
2024-01-22 19:37:05 +0200

    create error mw

--- utils/signup.validator.js
+++ utils/signup.validator.js
@@ -11,17 +11,17 @@ const schema = {
         last_name: { type: "string", minLength: 3 },
         email: { type: "string", pattern: "^.+\@.+\..+$" },
         password: { type: "string", pattern: "^(?=.*[A-Za-z])(?=.*\\d)[A-Za-z\\d]{8,}$" },
-        confirmPassword: { type: "string", const: { $data: "1/password" } },
+        confirm_password: { type: "string", const: { $data: "1/password" } },
 
     },
-    required: ["first_name", "last_name","email", "password", "confirmPassword"],
+    required: ["first_name", "last_name","email", "password", "confirm_password"],
     errorMessage: {
         properties: {
             first_name: "first name is too short",
             last_name: "last name is too short",
             email: "email is not valid",
             password: 'password is not valid',
-            confirmPassword: "must match with password",
+            confirm_password: "must match with password",
         },
     },
 }
