08481a9a881593708b0dfcdca4e25b1759ceff5c
Youssef Hussein <61589133+just-youssef@users.noreply.github.com>
2024-01-25 23:08:21 +0200

    update cloudinary uploadFile route

--- controllers/cloudinary.controller.js
+++ controllers/cloudinary.controller.js
@@ -1,24 +1,14 @@
 import { v2 as cloudinary } from 'cloudinary' ;
-import User from '../models/user.model.js';
 
-const changeAvatar = async(req, res, nxt) => {
+const uploadFile = async(req, res, nxt) => {
     try {
-        const { url } = await cloudinary.uploader.upload(req.file.path, { folder: `${req.userID}/avatars` });
-        
-        // update user avatar
-        let user = await User.findById(req.userID);
-        user.avatar = url;
-        await user.save();
-        console.log('avatar changed');
-
-        // exclude password and send user data
-        const { password: pass, ...rest } = user._doc;
-        return res.json(rest);
+        const { url } = await cloudinary.uploader.upload(req.file.path, { folder: `${req.userID}` });
+        return res.json({ url });
     } catch (err) {
         nxt(err)
     }
 }
 
 export {
-    changeAvatar,
+    uploadFile,
 }
