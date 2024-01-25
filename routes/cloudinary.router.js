269d9edfd851152f9de0b8ae14eee5652b20903a
Youssef Hussein <61589133+just-youssef@users.noreply.github.com>
2024-01-25 23:08:59 +0200

    update cloudinary uploadFile route

--- routes/cloudinary.router.js
+++ routes/cloudinary.router.js
@@ -1,11 +1,11 @@
 import { Router } from "express";
 import AuthMW from "../middlewares/auth.mw.js";
-import { changeAvatar } from "../controllers/cloudinary.controller.js";
+import { uploadFile } from "../controllers/cloudinary.controller.js";
 import multer from "multer";
 
 const multerUploadMW = multer({ dest: 'multer/' })
 const router = Router();
 
-router.post('/changeAvatar', [AuthMW, multerUploadMW.single('avatar')], changeAvatar)
+router.post('/uploadFile', [AuthMW, multerUploadMW.single('file')], uploadFile)
 
 export default router;
